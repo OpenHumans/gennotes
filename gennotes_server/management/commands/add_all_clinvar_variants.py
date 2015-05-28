5746014b187b19e9858ad852176cd162637f2e0b
Madeleine Ball <mpball@gmail.com>
2015-05-28 14:04:28 -0400

    Add management command to import ClinVar variants

---
+++ gennotes_server/management/commands/add_all_clinvar_variants.py
@@ -0,0 +1,67 @@
+from ftplib import FTP
+import gzip
+import os
+import re
+import shutil
+import tempfile
+
+from django.core.management.base import BaseCommand, CommandError
+
+from gennotes_server.models import map_chrom_to_index, Variant
+
+CV_VCF_DIR = 'pub/clinvar/vcf_GRCh37'
+CV_VCF_REGEX = r'^clinvar_[0-9]{8}.vcf.gz$'
+
+
+class Command(BaseCommand):
+    help = 'Download latest ClinVar VCF, import variants not already in db.'
+
+    def _download_latest_clinvar(self, dest_dir):
+        ftp = FTP('ftp.ncbi.nlm.nih.gov')
+        ftp.login()
+        ftp.cwd(CV_VCF_DIR)
+        cv_vcf_w_date = [f for f in ftp.nlst() if re.match(CV_VCF_REGEX, f)]
+        if len(cv_vcf_w_date) != 1:
+            raise CommandError('ClinVar reporting more than one VCF matching' +
+                               ' regex: \'{0}\' in directory {1}'.format(
+                                   CV_VCF_REGEX, CV_VCF_DIR))
+        ftp_vcf_filename = cv_vcf_w_date[0]
+        dest_filepath = os.path.join(dest_dir, ftp_vcf_filename)
+        with open(dest_filepath, 'w') as fh:
+            ftp.retrbinary('RETR {0}'.format(ftp_vcf_filename), fh.write)
+        return dest_filepath
+
+    def handle(self, *args, **options):
+        tempdir = tempfile.mkdtemp()
+        print "Created tempdir {}".format(tempdir)
+        clinvar_fp = self._download_latest_clinvar(tempdir)
+        print "Downloaded latest Clinvar, stored at {}".format(clinvar_fp)
+        clinvar_vcf = gzip.open(clinvar_fp)
+        new_variants = []
+        for line in clinvar_vcf:
+            if line.startswith('#'):
+                continue
+            data = line.rstrip('\n').split('\t')
+            chromosome = map_chrom_to_index(data[0])
+            pos = int(data[1])
+            ref_allele = data[3]
+            var_alleles = data[4].split(',')
+            all_alleles = [ref_allele] + var_alleles
+            info_dict = {v[0]:v[1] for v in
+                         [x.split('=') for x in data[7].split(';')]
+                         if len(v) == 2}
+            for allele in info_dict['CLNALLE'].split(','):
+                var_allele = all_alleles[int(allele)]
+                matched_var = Variant.objects.filter(chromosome=chromosome,
+                                                     pos=pos,
+                                                     ref_allele=ref_allele,
+                                                     var_allele=var_allele)
+                if not matched_var.exists():
+                    variant = Variant(chromosome=chromosome,
+                                      pos=pos,
+                                      ref_allele=ref_allele,
+                                      var_allele=var_allele)
+                    new_variants.append(variant)
+        print "Adding {0} new variants to database".format(len(new_variants))
+        Variant.objects.bulk_create(new_variants)
+        shutil.rmtree(tempdir)
---
+++ gennotes_server/models.py
@@ -0,0 +1,55 @@
+from collections import OrderedDict
+
+from django.db import models
+
+CHROMOSOMES = OrderedDict([
+    (1, 'chr1'),
+    (2, 'chr2'),
+    (3, 'chr3'),
+    (4, 'chr4'),
+    (5, 'chr5'),
+    (6, 'chr6'),
+    (7, 'chr7'),
+    (8, 'chr8'),
+    (9, 'chr9'),
+    (10, 'chr10'),
+    (11, 'chr11'),
+    (12, 'chr12'),
+    (13, 'chr13'),
+    (14, 'chr14'),
+    (15, 'chr15'),
+    (16, 'chr16'),
+    (17, 'chr17'),
+    (18, 'chr18'),
+    (19, 'chr19'),
+    (20, 'chr20'),
+    (21, 'chr21'),
+    (22, 'chr22'),
+    (23, 'chrX'),
+    (24, 'chrY'),
+    (25, 'chrM'),
+    ])
+
+
+def map_chrom_to_index(chrom_str):
+    if chrom_str.startswith('chr' or 'Chr'):
+        chrom_str = chrom_str[3:]
+    if chrom_str.startswith('ch' or 'Ch'):
+        chrom_str = chrom_str[2:]
+    try:
+        return int(chrom_str)
+    except ValueError:
+        if chrom_str == 'X':
+            return 23
+        elif chrom_str == 'Y':
+            return 24
+        elif chrom_str in ['M', 'MT']:
+            return 25
+    raise ValueError("Can't determine chromosome for {0}".format(chrom_str))
+
+
+class Variant(models.Model):
+    chromosome = models.PositiveSmallIntegerField(choices=CHROMOSOMES.items())
+    pos = models.PositiveIntegerField()
+    ref_allele = models.CharField(max_length=255)
+    var_allele = models.CharField(max_length=255)
